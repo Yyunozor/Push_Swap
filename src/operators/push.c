73365168709255b14fa0525d3a4110e84d8f7db3
Yyunozor <yyunozor@icloud.com>
2025-04-15 21:34:33 +0200

    add push funcs file

--- src/operators/push.c
+++ src/operators/push.c
@@ -6,9 +6,25 @@
 /*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 19:20:56 by anpayot           #+#    #+#             */
-/*   Updated: 2025/04/14 21:51:31 by anpayot          ###   ########.fr       */
+/*   Updated: 2025/04/15 21:32:46 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
+void	push(t_stack *src, t_stack *dest, char stack_name)
+{
+	int i;
+
+	if (src->size == 0)
+		return ;
+	
+	i = dest->size;
+	while (i > 0)
+	{
+		dest->array[i] = src->array[i - 1];
+		i--;
+	}
+
+	
+}
