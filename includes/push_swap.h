b7bdaa595bc97f111166dbd3df61dfc5eea82fda
Yyunozor <yyunozor@icloud.com>
2025-04-17 01:53:37 +0200

    add limits.h in ps.h

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: anpayot <anpayot@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 18:35:08 by anpayot           #+#    #+#             */
-/*   Updated: 2025/04/16 22:44:27 by anpayot          ###   ########.fr       */
+/*   Updated: 2025/04/17 01:10:19 by anpayot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,13 @@
 # define PUSH_SWAP_H
 
 # include "../ft_printf2/includes/ft_printf.h"
-#include "push_swap_doc.h"
-
-/* Integer limits */
-# define INT_MIN -2147483648
-# define INT_MAX 2147483647
+# include "push_swap_doc.h"
+# include <limits.h>
 
 /* Function prototypes */
 // Parser functions
 int		validation(int ac, char **av);
 t_stack	*parse_args(int ac, char **av);
-t_stack	*stack_init(int capacity);
 
 // Stack operations
 void	pa_pb(t_stack *src, t_stack *dest, char stack_name);	// pa, pb
@@ -36,6 +32,7 @@ void	rr(t_stack *stack_a, t_stack *stack_b);					// rr
 void	rrr(t_stack *stack_a, t_stack *stack_b);				// rrr
 
 // Utility functions
+t_stack	*stack_init(int capacity);
 int		stack_size(t_stack *stack);
 void	free_stack(t_stack **stack);
 void	print_stack(t_stack *stack);
